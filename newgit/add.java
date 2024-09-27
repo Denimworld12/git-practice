70784cf9c487259aee7295dcee41b1433d5b4e90
Nikhil Gupta <nikhilrakeshg@gmail.com>
2024-09-27 18:22:03 +0530

    new changes

--- newgit/add.java
+++ newgit/add.java
@@ -6,6 +6,6 @@ import javax.swing.SpringLayout;
 
 public class add {
     public static void main(String[] args) {
-    System.out.println(34+66);
+    System.out.println(34+66+"this is new ");
     }
 }
